e6c1e761adf44e2ec1248cfc19fbc35eb4ec8cde
Jonathan Hsieh <jonathanh1386@gmail.com>
2023-09-11 02:54:11 -0700

    merged logger into branch

--- src/client.py
+++ src/client.py
@@ -2,13 +2,15 @@ import discord
 import re
 import ws_listener as ws
 
+from loguru import logger
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 def is_dfa_link(link: str) -> bool:
-  return re.match(r'https://downforacross.com/beta/game/.+', link)
+  return re.match(r'https://downforacross.com/beta/game/.+', link) is not None
 
-def get_dfa_code(link: str) -> bool:
+def get_dfa_code(link: str) -> str:
   return re.sub(r'https://downforacross.com/beta/game/', '', link)
 
 class CrossworkerClient(discord.Client):
@@ -17,6 +19,7 @@ class CrossworkerClient(discord.Client):
 
   async def on_message(self, message: discord.Message):
     if message.author == self.user:
+      # ignore messages sent by the bot
       return
     
     if is_dfa_link(message.content):
@@ -27,7 +30,6 @@ class CrossworkerClient(discord.Client):
       client.join_game(dfa_code)
       client.listen_game(dfa_code)
 
-    
     # await message.channel.send(f"Hi, {message.author}")
 
   
