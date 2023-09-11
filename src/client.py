59aa866dcc661a4874b127367e5fc816d53f828c
Jonathan Hsieh <jonathanh1386@gmail.com>
2023-09-11 03:45:37 -0700

    started listening to messages

--- src/client.py
+++ src/client.py
@@ -1,6 +1,7 @@
 import discord
 import re
-import ws_listener as ws
+from socker_handler import WebsocketClient
+import asyncio
 
 from loguru import logger
 
@@ -17,18 +18,25 @@ class CrossworkerClient(discord.Client):
   async def on_ready(self):
     print(f'Logged on as {self.user}!')
 
+    dfa_code = "3853980-bruss"
+    print(f'Joining game {dfa_code}')
+    await WebsocketClient().join_game(dfa_code)
+
   async def on_message(self, message: discord.Message):
     if message.author == self.user:
       # ignore messages sent by the bot
       return
     
-    if is_dfa_link(message.content):
-      dfa_code = get_dfa_code(message.content)
+    if is_dfa_link(message.content) or True:
+      # dfa_code = get_dfa_code(message.content)
+      dfa_code = "3853980-bruss"
       print(f'Joining game {dfa_code}')
       await message.channel.send(f'Joining crossword {dfa_code}!')
-      client = ws.WebsocketClient()
-      client.join_game(dfa_code)
-      client.listen_game(dfa_code)
+      await WebsocketClient().join_game(dfa_code)
+
+      # client = ws.WebsocketClient()
+      # client.join_game(dfa_code)
+      # client.listen_game(dfa_code)
 
     # await message.channel.send(f"Hi, {message.author}")
 
