ec87ad168976aaa3340ef8952bb7c20a5918498e
TrevorGuo <trevorguo@gmail.com>
2023-09-11 17:38:18 -0700

    Actually staged changes

--- src/client.py
+++ src/client.py
@@ -1,12 +1,15 @@
 import discord
 import re
-import websocket
+import ws_listener as ws
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 def is_dfa_link(link: str) -> bool:
   return re.match(r'https://downforacross.com/beta/game/.+', link)
+
+def get_dfa_code(link: str) -> bool:
+  return re.sub(r'https://downforacross.com/beta/game/', '', link)
 
 class CrossworkerClient(discord.Client):
   async def on_ready(self):
@@ -16,7 +19,16 @@ class CrossworkerClient(discord.Client):
     if message.author == self.user:
       return
     
-    await message.channel.send(f"Hi, {message.author}")
+    if is_dfa_link(message.content):
+      dfa_code = get_dfa_code(message.content)
+      print(f'Joining game {dfa_code}')
+      await message.channel.send(f'Joining crossword {dfa_code}!')
+      client = ws.WebsocketClient()
+      client.join_game(dfa_code)
+      client.listen_game(dfa_code)
+
+    
+    # await message.channel.send(f"Hi, {message.author}")
 
   
     
--- src/ws_listener.py
+++ src/ws_listener.py
@@ -1,36 +1,30 @@
-import socketio
-import traceback
 import websocket
 import time
-
-# standard Python
-# sio = socketio.Client()
-
-# @sio.event
-# def connect():
-#     print("I'm connected!")
-
-# @sio.event
-# def message(data):
-#     print('I received a message!')
-#     print(data)
-
-# sio.connect('wss://api.foracross.com/socket.io/?EIO=3&transport=websocket')
-
-lobby_id = '3850420-phrint'
-
-websocket.enableTrace(True)
-ws = websocket.WebSocket()
-ws.connect("wss://api.foracross.com/socket.io/?EIO=3&transport=websocket")
-print(ws.recv())
-print(ws.recv())
-ws.send(f'420["join_game", "{lobby_id}"]')
-print(ws.recv())
-ws.send(f'421["sync_all_game_events","{lobby_id}"]')
-print(ws.recv())
-
-while True:
-    print(ws.recv())
-    ws.send('2')
-    time.sleep(1.5)
     
+class WebsocketClient():
+    def __init__(self):
+        self.websocket_url = 'wss://api.foracross.com/socket.io/?EIO=3&transport=websocket'
+    def join_game(self, game_id):
+        self.ws = websocket.WebSocket()
+
+        self.ws.connect(self.websocket_url)
+        print(self.ws.recv())
+        print(self.ws.recv())
+        self.ws.send(f'420["join_game", "{game_id}"]')
+        print(self.ws.recv())
+        self.ws.send(f'421["sync_all_game_events","{game_id}"]')
+        print(self.ws.recv())
+
+        print('Joined game')
+
+    def listen_game(self, game_id):
+        last_ping = time.time()
+        while True:
+            if time.time() - last_ping > 1.5:
+                print("I'm sending a msg")
+                self.ws.send('2')
+                last_ping = time.time()
+            try:
+                print(self.ws.recv())
+            except:
+                continue
