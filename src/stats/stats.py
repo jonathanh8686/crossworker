98b6d2289914de4f2dd2643a9455a1cdc9609dd7
Jonathan Hsieh <jonathanh1386@gmail.com>
2023-09-17 06:18:53 -0700

    so free

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from src.stats.correct_map import get_correct_map
 
+from celluloid import Camera
 from ..discord_bot.message_types import GameEvent, GameModel
 
 
@@ -12,10 +13,13 @@ class Statistics:
         self.history = history
 
     def get_visualization(self):
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+        camera = Camera(fig)
         for update in self.history["updateCell"]:
-            fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
             next(
                 get_correct_map(ax2, self.game, self.history, update.timestamp)
             )
-
-            fig.savefig(f"test_images/{update.timestamp}.png")
+            camera.snap()
+        
+        animation = camera.animate()
+        animation.save('test.mp4')
